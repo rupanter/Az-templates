17495d82eab68ea562fd14ad8c6f155618fa350d
Rupanter Chhabra <rupanterchhabra@Rupanters-MBP.guest.corp.microsoft.com>
2019-06-06 18:54:56 +0530

    Uploading more Policies

--- AzPolicies/Deploy-RedirectionPolicy.ps1
+++ AzPolicies/Deploy-RedirectionPolicy.ps1
@@ -1,20 +1,36 @@
-param(
-    [Parameter(Mandatory=$True)]
+<#
+> Example :
+.\Deploy-RedirectionPolicy.ps1 -SubscriptionID "f28f2c24-XXXX-XXXX-XXXX-0ef3r09c0578" -WorkspaceName "BasictestingOMS" -WorkspaceID "c9098c5c-XXXX-XXXX-XXXX-fd99fyu6e134" -Policyfile "Demo/MMA-Installation-Windows-TagONLY-Policy.rules.JSON" -Parameterfile "Demo/MMA-Installation-Windows-TagONLY-Policy.parameters.JSON"
+
+Author :
+Rupanter Chhabra
+Rupanter.Chhabra@microsoft.com
+Microsoft Corporation
+#>
+Param(
+    [Parameter(Mandatory = $True)]
     [String]
     $SubscriptionID,
-    [Parameter(Mandatory=$True)]
+    [Parameter(Mandatory = $True)]
     [String]
-    $LogAnalytics,
-    [Parameter(Mandatory=$True)]
+    $WorkspaceName,
+    [Parameter(Mandatory = $True)]
     [String]
     $workspaceID,
-    [Parameter(Mandatory=$False)]
+    [Parameter(Mandatory = $False)]
     [String]
-    $location
+    $location,
+    [Parameter(Mandatory = $True)]
+    [String]
+    $Policyfile,
+    [Parameter(Mandatory = $True)]
+    [String]
+    $Parameterfile
 )
 $location = 'eastus'
-$definition = New-AzPolicyDefinition -Name "Redirection-MMA-Windows" -DisplayName "Redirection-MMA-Windows-Definition" -description "This policy redirects the Microsoft Monitoring Agent to a specified Log Analytics Workspace." -Policy 'https://msazure.visualstudio.com/One/_git/AzureCXP-AME-LoadTest?path=%2FDemo%2FMMA-Redirection-Windows-Policy.rules.JSON&version=GBruchh' -Parameter 'https://msazure.visualstudio.com/One/_git/AzureCXP-AME-LoadTest?path=%2FDemo%2FMMA-Redirection-Windows-Policy.parameters.JSON&version=GBruchh' -Mode All
+#Update -Policy and -Parameter
+$definition = New-AzPolicyDefinition -Name "Redirection-MMA-Windows" -DisplayName "Redirection-MMA-Windows-Definition" -description "This policy redirects the Microsoft Monitoring Agent to a specified Log Analytics Workspace." -Policy $Policyfile -Parameter $Parameterfile -Mode All
 $definition
 $Subscription = "/subscriptions/" + $SubscriptionID
-$assignment = New-AzPolicyAssignment -Name "Redirection-MMA-Windows-Assignment" -PolicyDefinition $Definition -Scope $Subscription -AssignIdentity -location $location -logAnalytics $loganalytics -workspaceID $workspaceID
+$assignment = New-AzPolicyAssignment -Name "Redirection-MMA-Windows-Assignment" -PolicyDefinition $Definition -Scope $Subscription -AssignIdentity -location $location -logAnalytics $WorkspaceName -workspaceID $workspaceID
 $assignment
